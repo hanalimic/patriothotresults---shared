2e2de008302e793cda95f2d60d68a15d34a53c32
kvidakov <vidakovic.karlo@gmail.com>
2021-07-06 17:23:06 +0200

    refactor(index): import and export

--- index.ts
+++ index.ts
@@ -1,3 +1,37 @@
-export * from "./components/index";
-export * from "./interfaces/index";
-export * from "./services/index";
+// import components
+import { Footer } from "./components/index";
+import { Header } from "./components/index";
+import { HeroSection } from "./components/index";
+import { LiveStream } from "./components/index";
+import { Schoresheet } from "./components/index";
+import { Sponsors } from "./components/index";
+
+//import interfaces
+import { IDivision } from "./interfaces/index";
+import { IMatch } from "./interfaces/index";
+import { TournamentParams } from "./interfaces/index";
+
+// import services
+import { divisionService } from "./services/index";
+import { matchService } from "./services/index";
+import { sponsorsService } from "./services/index";
+import { tournamentService } from "./services/index";
+
+// export components
+export { Footer };
+export { Header };
+export { HeroSection };
+export { LiveStream };
+export { Schoresheet };
+export { Sponsors };
+
+// export interfaces
+export { IDivision };
+export { IMatch };
+export { TournamentParams };
+
+// export services
+export { divisionService };
+export { matchService };
+export { sponsorsService };
+export { tournamentService };
